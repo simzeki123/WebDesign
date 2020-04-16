1eb14ed086c815bac57ce93fde8d02625a320a74
Tiffceet <zkang.loo@gmail.com>
2020-04-17 00:07:30 +0800

    Added functionality to pretain search_query upon search

--- js/header.js
+++ js/header.js
@@ -13,23 +13,33 @@ $(document).ready(function () {
 			);
 		});
 
-        // I will set the margin too on load
-        $(".drop-down-container").css(
-            "right",
-            `${Math.round(
-                window.innerWidth * 0.1 +
-                    (window.innerWidth * 0.8 * 0.25 - 48) / 2
-            )}px`
-        );
+		// I will set the margin too on load
+		$(".drop-down-container").css(
+			"right",
+			// I don't know why, i don't know how, if it works, dont touch it
+			`${Math.round(
+				window.innerWidth * 0.1 +
+					(window.innerWidth * 0.8 * 0.25 - 48) / 2
+			)}px`
+		);
+		// set right margin on window resize to ensure correct position
 		$(window).on("resize", function () {
 			$(".drop-down-container").css(
 				"right",
+				// I don't know why, i don't know how, if it works, dont touch it
 				`${Math.round(
 					window.innerWidth * 0.1 +
 						(window.innerWidth * 0.8 * 0.25 - 48) / 2
 				)}px`
 			);
 		});
+
+		// If searcH_query exist, make sure the search input box have the correct value
+        let params = new URLSearchParams(location.search);
+        if (params.get("search_query") != null)
+        {
+            $("#search_query").val(params.get("search_query"));
+        }
 
 		// Hope this doesnt override on click in popupHandler
 		$("#openLoginBox").on("click", function () {
@@ -40,7 +50,7 @@ $(document).ready(function () {
 			$(".drop-down-container").css("display", "none");
 		});
 
-        // If Login was done before hand
+		// If Login was done before hand
 		if (sessionStorage.getItem("login") == "true") {
 			let disabled = $(".drop-disabled");
 			let enabled = $(".drop-enabled");
@@ -48,8 +58,8 @@ $(document).ready(function () {
 			disabled.addClass("drop-enabled").removeClass("drop-disabled");
 			$("#logOutBtn").css("display", "block");
 			$("#logOutBtn").on("click", function () {
-                alert("You are being logged out.");
-                sessionStorage.setItem("login", false);
+				alert("You are being logged out.");
+				sessionStorage.setItem("login", false);
 				window.location.href = window.location.href.substring(
 					0,
 					window.location.href.indexOf("?")
